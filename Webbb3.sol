810e39c5c33ca1b08c4d2a56c67dca503eb0dd30
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-29 20:19:27 -0300

    Update Webbb3.sol

--- Webbb3.sol
+++ Webbb3.sol
@@ -3,18 +3,31 @@ pragma solidity ^0.8.21;
 
 // Definindo uma estrutura para representar uma votação
 struct Voting {
-    string option1; // Opção 1 para votar
-    uint votes1;    // Total de votos para a opção 1
-    string option2; // Opção 2 para votar
-    // Não há variável para armazenar os votos para a opção 2, o que pode ser um erro de implementação
+    string option1; 
+    uint votes1;    
+    string option2; 
+    uint vote2;
+    uint maxDate;
 }
 
 contract Webbb3 {
-    address owner;        // Endereço do proprietário do contrato
-    uint public currentVoting = 0; // Contador para o número atual de votações
+    address owner;      
+    uint public currentVoting = 0; 
+    Voting [] public votings;
 
-    // Função construtora do contrato
     constructor() {
-        owner = msg.sender; // Define o endereço do proprietário como o remetente da transação que implanta o contrato
+        owner = msg.sender;
+    }
+
+    function addVoting(string memory option1, string memory option2, uint timeToVote) public {
+       require(msg.sender == owner, "invalid Sender");
+
+       if(currentVoting != 0) currentVoting++;
+
+        Voting memory newVoting;
+        newVoting.option1 = option1;
+        newVoting.option2 = option2;
+        newVoting.maxDate = timeToVote + block.timestamp;
+        votings.push(newVoting);
     }
 }
