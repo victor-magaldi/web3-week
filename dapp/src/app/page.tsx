eb528b5338479b29b1e0eb210e82c237bb206ac6
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-02-01 20:35:43 -0300

    create footer and img

--- dapp/src/app/page.tsx
+++ dapp/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     console.log("handleClick");
   };
   return (
-    <>
+    <div>
       <Head>
         <title>Webbb3 | Login</title>
         <meta charSet="utf-8" />
@@ -35,12 +35,36 @@ export default function Home() {
                 className="btn btn-primary btn-lg px-4 me-md-2"
                 onClick={handleClick}
               >
-                Entrar
+                <img
+                  src="/metamask.svg"
+                  width={64}
+                  alt="Conectar com Metamask"
+                  className="me-3"
+                  title="Conectar com Metamask"
+                />
+                Conectar com Metamask
               </button>
             </div>
           </div>
         </div>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <p className="col-md-4 mb-0 text-body-secondary">
+            &copy; 2024 Webbb3, INC
+          </p>
+          <ul className="nav col-md-4 justify-content-end">
+            <li className="nav-item">
+              <a href="/" className="nav-link px-2 text-body-secondary">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/about" className="nav-link px-2 text-body-secondary">
+                About
+              </a>
+            </li>
+          </ul>
+        </footer>
       </div>
-    </>
+    </div>
   );
 }
