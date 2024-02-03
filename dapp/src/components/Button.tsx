2d3b90c8cef1dad1ccca266fb206376142c82475
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-02-03 20:00:26 -0300

    connect wallet

--- dapp/src/components/Button.tsx
+++ dapp/src/components/Button.tsx
@@ -6,24 +6,32 @@ export function Button() {
   const [message, setMessage] = useState("");
 
   const handleClick = async () => {
-    const wallet = await login();
-    console.log(wallet);
+    try {
+      await login();
+      setMessage("Conectado na Carteira");
+    } catch (err) {
+      setMessage("não foi possível se conectar em sua carteira");
+      throw new Error(`err==>${err}`);
+    }
   };
   return (
-    <button
-      type="button"
-      className="btn btn-primary btn-lg px-4 me-md-2"
-      onClick={handleClick}
-    >
-      <img
-        src="/metamask.svg"
-        width={64}
-        alt="Conectar com Metamask"
-        className="me-3"
-        title="Conectar com Metamask"
-      />
-      Conectar com Metamask
+    <div className="d-flex flex-column bd-highlight mb-3">
+      <button
+        type="button"
+        className="btn btn-primary btn-lg px-4 me-md-2"
+        onClick={handleClick}
+      >
+        <img
+          src="/metamask.svg"
+          width={64}
+          alt="Conectar com Metamask"
+          className="me-3"
+          title="Conectar com Metamask"
+        />
+        Conectar com Metamask
+      </button>
+      <br />
       <p className="message">{message}</p>
-    </button>
+    </div>
   );
 }
